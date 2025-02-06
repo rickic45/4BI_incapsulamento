cf240a6140537a00d46bf1086a396cf292e3150c
rickic45 <riccardocroccolo2007@gmail.com>
2025-02-06 18:37:18 +0100

    Update funzioni.h

---
+++ funzioni.h
@@ -0,0 +1,30 @@
+#ifndef FUNZIONI_H
+#define FUNZIONI_H
+#include <string>  
+using namespace std;  
+
+struct IpHeader{
+	string Version;
+	string Hlen;
+	int TOS;
+	string TOTlenght;
+	int ID;
+	string Flags;
+	int Fragmentoffset;
+	string TTL;
+	string Protocol;
+	int Headerchecksum;
+	long SIA;
+	long DIA;
+	string Options;
+	int Padding;
+};
+
+void Datagram();
+string leggiMessaggio();
+void scritturaMessaggio(string carattere, string stringa, string messaggioBin);
+string charToBin(char value);
+string stringToBin(string input);
+string messaggioToBin(string input);
+
+#endif
