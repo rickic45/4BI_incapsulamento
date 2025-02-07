763c275b9ed9344913ac15cb2f4f49b6d4d0a35f
rickic45 <riccardocroccolo2007@gmail.com>
2025-02-07 09:27:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ using namespace std;
 int main(){
     string messaggio = leggiMessaggio();
     string messaggioBin = messaggioToBin(messaggio);
-    string carattere = charToBin('A');
-    string stringa = stringToBin("ciao");
-    scritturaMessaggio(carattere, stringa, messaggioBin);
+    scritturaMessaggio(messaggioBin);
     return 0;
 }
