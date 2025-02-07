13b43286291d64c2a3a91c3d514fa240757d2295
rickic45 <riccardocroccolo2007@gmail.com>
2025-02-07 10:41:15 +0100

    Update funzioni.cpp

--- funzioni.cpp
+++ funzioni.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // Funzione che inizializza i valori dell'header IP
-void Datagram(){
+IpHeader Datagram(){
 	IpHeader campi;
 	campi.Version = "0100";
 	campi.Hlen = "0101";
@@ -18,7 +18,26 @@ void Datagram(){
 	campi.Protocol ="00000110";
 	campi.Headerchecksum = "0000000000000000";
 	campi.SIA = "11000000101010000000000100001010";
-	campi.DIA = "00001000000010000000000000001000"; 
+	campi.DIA = "00001000000010000000000000001000";
+	campi.Data =  messaggioToBin(leggiMessaggio());
+	return campi;
+}
+
+
+string  concatIpHeader(const IpHeader& header){
+       return header.Version + header.Hlen + header.TOS + header.TOTlenght +
+           header.ID + header.Flags + header.Fragmentoffset + header.TTL +
+           header.Protocol + header.Headerchecksum + header.SIA +
+           header.DIA + header.Data;
+}
+
+void CreaFrame(){
+	EthernetFrame campi;
+	campi.Preamble = "1010101010101010101010101010101010101010101010101010101010101010";
+	campi.Sfd = "11010101";
+	campi.Da = "101011111010111110101111101011111010111110101111";
+	campi.Sa = "101011001010111110101101101011001010011010101110";
+	campi.Type = "10000000000000000";
 }
 
 // Funzione per leggere il messaggio da un file
@@ -37,13 +56,14 @@ string leggiMessaggio(){
 }
 
 // Funzione per scrivere il messagio su file 
-void scritturaMessaggio(string messaggioBin){
+void scritturaMessaggio(string messaggioBin, string ToTframe){
     ofstream fout("frame.txt");
     if (!fout){
         cout << "Errore!!!! non ho scritto nulla, riga 22 :((" << endl;
         exit(1); 
     }
-    fout << messaggioBin << endl;
+    fout << "il messaggio in binario e': " << messaggioBin << endl;
+    fout << "il frame in binario e': " << ToTframe << endl;
     fout.close();
 }
 
@@ -95,15 +115,5 @@ string messaggioToBin(string input){
     return result;
 }
 
-string calcolaCRC(const string &payload){
-    unsigned short crc = 0xFFFF;
-    const unsigned short POLY = 0x1021;
-    for (unsigned char c : payload){
-        crc ^= (c << 8);
-        for (int i = 0; i < 8; i++){
-            if (crc & 0x8000) crc = (crc << 1) ^ POLY;
-            else crc <<= 1;
-        }
-    }
-    return crc;
-}
+
+
