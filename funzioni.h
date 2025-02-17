a97fb8aa2f6ec315b96830da1c7355692bf9ad6f
rickic45 <riccardocroccolo2007@gmail.com>
2025-02-17 18:37:50 +0100

    Update funzioni.h

--- funzioni.h
+++ funzioni.h
@@ -3,40 +3,40 @@
 #include <string>  
 using namespace std;  
 
-struct IpHeader{
-	string Version;
-	string Hlen;
-	string TOS;
-	string TOTlenght;
-	string ID;
-	string Flags;
-	string Fragmentoffset;
-	string TTL;
-	string Protocol;
-	string Headerchecksum;
-	string SIA;
-	string DIA;
-	string Options;
-	string Padding;
-	string Data;
+struct IpHeader {
+    string Version;
+    string Hlen;
+    string TOS;
+    string TOTlenght;
+    string ID;
+    string Flags;
+    string Fragmentoffset;
+    string TTL;
+    string Protocol;
+    string Headerchecksum;
+    string SIA;
+    string DIA;
+    string Data;
 };
 
-struct EthernetFrame{
-	string Preamble;
-	string Sfd;
-	string Da;
-	string Sa;
-	string Type;
-	IpHeader ipHeader;
+struct EthernetFrame {
+    string Preamble;
+    string Sfd;
+    string Da;
+    string Sa;
+    string Type;
+    IpHeader ipHeader;
 };
 
 IpHeader Datagram();
 string leggiMessaggio();
-void scritturaMessaggio(string messaggioBin, string ToTframe);
+void scritturaMessaggio(string Totframelvl2);
 string charToBin(char value);
 string stringToBin(string input);
 string messaggioToBin(string input);
 string concatIpHeader(const IpHeader& header);
+string concatFrameHeader(const EthernetFrame& header);
+EthernetFrame CreaFrame();
+string ipChecksum(const string& bits);
 
 #endif
-
